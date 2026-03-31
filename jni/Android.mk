bd30a2478197ad52cf2032205115244aff1b2d6f
invuzt <invuzt@gmail.com>
2026-03-31 10:20:24 +0700

    fix: final switch to Java-Rust JNI bridge

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,15 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := cakru_core
-LOCAL_SRC_FILES := libcakru_core.a
-include $(PREBUILT_STATIC_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := hello
-LOCAL_SRC_FILES := hello.c sound.c
-LOCAL_LDLIBS := -llog -landroid -lGLESv2 -lEGL -lOpenSLES
-LOCAL_STATIC_LIBRARIES := android_native_app_glue cakru_core
+LOCAL_MODULE    := hello
+LOCAL_SRC_FILES := hello.c
+LOCAL_LDLIBS    := -llog -landroid
+
+# Hubungkan dengan library Rust
+LOCAL_STATIC_LIBRARIES := cakru_core_static
+
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,android/native_app_glue)
+# Deklarasi Library Rust sebagai prebuilt
+include $(CLEAR_VARS)
+LOCAL_MODULE := cakru_core_static
+LOCAL_SRC_FILES := libcakru_core.a
+include $(PREBUILT_STATIC_LIBRARY)
--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,3 @@
 APP_ABI := arm64-v8a
 APP_PLATFORM := android-23
-
-LOCAL_CFLAGS += -Os -ffunction-sections -fdata-sections
-LOCAL_LDFLAGS += -Wl,--gc-sections
-
+APP_STL := c++_static
