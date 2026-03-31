d8ce30cee2ed85e377bdc4085756112ae99dafd0
invuzt <invuzt@gmail.com>
2026-03-31 07:56:59 +0700

    fix: match rust library name to cakru_core

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ NDK_PATH ?= $(ANDROID_NDK_HOME)
 build:
 	# 1. Build Rust Logic
 	cd rust_logic && cargo build --target aarch64-linux-android --release
-	# 2. Copy hasil build Rust ke folder jni agar ditemukan NDK
-	cp rust_logic/target/aarch64-linux-android/release/librust_logic.a jni/
+	# 2. Copy hasil build (Sesuaikan dengan nama di Cargo.toml)
+	cp rust_logic/target/aarch64-linux-android/release/libcakru_core.a jni/
 	# 3. Jalankan NDK Build
 	$(NDK_PATH)/ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=jni/Android.mk NDK_APPLICATION_MK=jni/Application.mk
--- jni/Android.mk
+++ jni/Android.mk
@@ -1,16 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := rust_logic
-# Kita asumsikan file ada di folder jni/ saat build
-LOCAL_SRC_FILES := librust_logic.a
+LOCAL_MODULE := cakru_core
+LOCAL_SRC_FILES := libcakru_core.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := hello
 LOCAL_SRC_FILES := hello.c sound.c
 LOCAL_LDLIBS := -llog -landroid -lGLESv2 -lEGL -lOpenSLES
-LOCAL_STATIC_LIBRARIES := android_native_app_glue rust_logic
+LOCAL_STATIC_LIBRARIES := android_native_app_glue cakru_core
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,android/native_app_glue)
